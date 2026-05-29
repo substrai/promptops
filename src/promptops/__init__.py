ce5f142a776db0f1370014a55347f285e18d1606
Gaurav Kumar Sinha <gaurav@substrai.dev>
2026-05-28 22:53:17 -0500

    chore: bump version to 0.8.0 for PyPI release
    Signed-off-by: Gaurav Kumar Sinha <gaurav@substrai.dev>

--- src/promptops/__init__.py
+++ src/promptops/__init__.py
@@ -11,7 +11,7 @@ Usage:
     result = client.invoke("summarize", inputs={"document": "...", "max_words": 100})
 """
 
-__version__ = "0.7.0"
+__version__ = "0.8.0"
 
 from promptops.core.prompt import Prompt, PromptDefinition
 from promptops.core.version import PromptVersion, VersionRange
